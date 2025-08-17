b4659cec94ad550e84b39e2d2981247a2486368a
Jta003 <jtakung@gmail.com>
2025-08-17 12:34:26 +0700

    Add githubPages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/My_To-Do_App/",  // 👈 ตรงนี้ใช้ชื่อ repo GitHub ของคุณ
 })
